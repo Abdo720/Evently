d773fddb810fa4d8887d83026f7e0b65345b00e0
Abdullah <abdullahbehary1@gmail.com>
2026-01-19 15:37:17 +0200

    start To Do

---
+++ lib/Screens/Splachscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Splachscreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => Splach();
+}
+class Splach extends State<Splachscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Spacer(),
+
+        Spacer(),
+      ],
+    );
+  }
+}
