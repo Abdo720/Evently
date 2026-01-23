46364383678f1e044a33fa88c1bdb376bb4f21f2
Abdullah <abdullahbehary1@gmail.com>
2026-01-23 13:27:28 +0200

    photos and other

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,15 @@ import 'package:to_do/Screens/IntroScreens/introscreenfirst.dart';
 import 'package:to_do/Screens/IntroScreens/introscreenlast.dart';
 import 'package:to_do/Screens/Splachscreen.dart';
 import 'package:to_do/Screens/introscreen_1.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main (){
+
+void main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
